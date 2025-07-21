eaeb51ccfe5fb112979f88d6e53b2feaf76d11e3
Martin Larsson <larssonmartin1998@gmail.com>
2025-07-21 13:20:12 +0200

    feat: Add Validate Config

--- src/config.zig
+++ src/config.zig
@@ -7,6 +7,10 @@ const GetCurrentError = error{
     Error,
 };
 
+const ValidateError = error{
+    ErrorsFound,
+};
+
 pub fn readAlloc(allocator: std.mem.Allocator, configDir: []const u8) !std.ArrayList([]const u8) {
     const configFile = try std.fs.openFileAbsolute(configDir, .{
         .mode = File.OpenMode.read_only,
@@ -65,9 +69,111 @@ pub fn getCurrent() ![]u8 {
     return GetCurrentError.Error;
 }
 
-pub fn validate() !void {
-    // Errors:
-    // multiple entries
-    // too long
-    // too big
+pub fn validateAlloc(allocator: std.mem.Allocator, entries: *std.ArrayList([]const u8)) !void {
+    var set = std.HashMap([]const u8, void, std.hash_map.StringContext, 10).init(allocator);
+    defer set.deinit();
+
+    try set.ensureTotalCapacity(10);
+
+    const stderr = std.io.getStdErr().writer();
+    var bw = std.io.bufferedWriter(stderr);
+    const writer = bw.writer();
+
+    var errcount: u16 = 0;
+    for (entries.items) |entry| {
+        const result_ptr = try set.getOrPut(entry);
+        if (result_ptr.found_existing) {
+            errcount += 1;
+            try writer.print("Error: Found duplicate entry in config \"{s}\"!\n", .{entry});
+        } else {
+            result_ptr.value_ptr.* = void{};
+        }
+    }
+
+    if (errcount == 0) {
+        try writer.print("Your config looks good, no errors found.\n", .{});
+    }
+
+    try bw.flush();
+
+    if (errcount > 0) {
+        return ValidateError.ErrorsFound;
+    }
+}
+
+test "Read Config" {
+    const cwd = std.fs.cwd();
+    cwd.makeDir("test") catch |err| switch (err) {
+        std.fs.Dir.MakeError.PathAlreadyExists => {},
+        else => return err,
+    };
+
+    const test_dir = try cwd.openDir("test", .{});
+    var test_config = test_dir.createFile("current", .{}) catch |err| switch (err) {
+        File.OpenError.PathAlreadyExists => try test_dir.openFile("current", .{}),
+        else => return err,
+    };
+    defer test_config.close();
+
+    const writer = test_config.writer();
+    var bw = std.io.bufferedWriter(writer);
+
+    const data = [_][]const u8{
+        "row1",
+        "row2",
+        "row3",
+    };
+
+    var buf: [16 * data.len]u8 = undefined;
+    for (0..data.len) |i| {
+        const start = i * 16;
+        const end = start + 16;
+        _ = try bw.write(try std.fmt.bufPrint(buf[start..end], "{s}" ++ "\n", .{data[i]}));
+    }
+
+    try bw.flush();
+
+    defer cwd.deleteDir("test") catch {};
+
+    const allocator = std.testing.allocator;
+    const test_conf_filepath_abs = try test_dir.realpathAlloc(allocator, "current");
+    defer allocator.free(test_conf_filepath_abs);
+
+    const entries = (try readAlloc(allocator, test_conf_filepath_abs));
+    defer {
+        for (entries.items) |entry| {
+            allocator.free(entry);
+        }
+        entries.deinit();
+    }
+
+    for (0..entries.items.len) |i| {
+        try std.testing.expectEqualStrings(data[i], entries.items[i]);
+    }
+}
+
+test "Validate config" {
+    const allocator = std.testing.allocator;
+    var entries = std.ArrayList([]const u8).init(allocator);
+    defer entries.deinit();
+
+    try entries.ensureTotalCapacity(10);
+    try entries.append("row1");
+    try entries.append("row2");
+    try entries.append("row3");
+
+    try validateAlloc(allocator, &entries);
+
+    var entries_with_duplicates = std.ArrayList([]const u8).init(allocator);
+    defer entries_with_duplicates.deinit();
+
+    try entries_with_duplicates.ensureTotalCapacity(10);
+    for (0..2) |_| {
+        for (entries.items) |entry| {
+            try entries_with_duplicates.append(entry);
+        }
+    }
+
+    const result = validateAlloc(allocator, &entries_with_duplicates);
+    try std.testing.expectEqual(result, ValidateError.ErrorsFound);
 }
