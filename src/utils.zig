e1d502a86bf04a8b43258ae258b1fd526873e1f3
Martin Larsson <larssonmartin1998@gmail.com>
2025-07-23 01:13:40 +0200

    Add isAlphanumericAscii to utils

--- src/utils.zig
+++ src/utils.zig
@@ -3,6 +3,19 @@ const c = @cImport({
     @cInclude("stdlib.h");
 });
 
+pub fn isAlphanumericAscii(str: []const u8) bool {
+    for (str) |char| {
+        if (!(('a' <= char and char <= 'z') or
+            ('A' <= char and char <= 'Z') or
+            ('0' <= char and char <= '9')))
+        {
+            return false;
+        }
+    }
+
+    return str.len > 0;
+}
+
 pub fn getEnv(env: [*c]const u8) std.process.GetEnvVarOwnedError![]u8 {
     const result_ptr = c.getenv(env);
     if (result_ptr == null) {
@@ -25,4 +38,30 @@ test "Heapless environment variable" {
     const stack_home_path = try getEnv("HOME");
 
     try std.testing.expectEqualStrings(heap_home_path, stack_home_path);
+}
+
+test "isAlphanumericAscii" {
+    for ([_][]const u8{
+        "lpq9oPwerASf",
+        "ASDASDQWEQWECNML",
+        "QWEUONNNAASD9321",
+        "WEhfsdDpCNDawe321",
+        "d",
+        "P",
+        "1",
+    }) |str| {
+        try std.testing.expect(isAlphanumericAscii(str));
+    }
+
+    for ([_][]const u8{
+        "",
+        ";.,",
+        "<>",
+        "!@#",
+        "%&*",
+        "tjenare!",
+        "åöä",
+    }) |str| {
+        try std.testing.expect(!isAlphanumericAscii(str));
+    }
 }
