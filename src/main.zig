5511160135c2468e2a4d00f40f5e6471262684f2
Martin Larsson <larssonmartin1998@gmail.com>
2025-07-22 16:15:38 +0200

    Increase memory threshold to support higher number of config entries.

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,7 @@ const utils = @import("utils.zig");
 const cli = @import("cli.zig");
 
 pub fn main() !void {
-    var buf: [512]u8 = undefined;
+    var buf: [2048]u8 = undefined;
     var fixed = std.heap.FixedBufferAllocator.init(&buf);
     const allocator = fixed.allocator();
 
